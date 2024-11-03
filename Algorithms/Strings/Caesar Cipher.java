d068e3bd7762d715cd69161fe5694a9fb34ce9b5
Tharunika L <tharunika.l14@gmail.com>
2024-11-03 22:21:03 +0530

    Create Caesar Cipher.java

---
+++ Algorithms/Strings/Caesar Cipher.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+    // Write your code here
+    k=k%26;
+    StringBuilder sb = new StringBuilder();
+    for(char c : s.toCharArray()){
+        if(Character.isUpperCase(c)){
+            sb.append((char)((c-'A'+k)%26 + 'A'));
+        }
+        else if(Character.isLowerCase(c)){
+            sb.append((char)((c-'a'+k)%26 + 'a'));
+        }
+        else{
+            
+            sb.append(c);
+        }
+    }return sb.toString();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
